23a5d0593229a51ff99d28abdd42886d37575a6c
KidusTadesseCode <Kidustadessearega@gmail.com>
2022-06-24 16:36:34 -0400

    index.sh dir change

--- index.sh
+++ index.sh
@@ -1,7 +1,8 @@
+CURRENT=$(ls -td -- * | head -n 1)
 echo "Do you wish to install git?"
 select GIT in "Yes" "No"; do
     case $GIT in
-        Yes )sh ~/git.sh; break;;
+        Yes )sh ~/$CURRENT/git.sh; break;;
         No ) break;;
         *) echo "Invalid entry. Please type 1 for yes or type 2 for no."
     esac
@@ -10,7 +11,7 @@ done
 echo "Do you wish to install nodejs?"
 select NODEJS in "Yes" "No"; do
     case $NODEJS in
-        Yes ) sh node.sh; break;;
+        Yes )sh ~/$CURRENT/node.sh; break;;
         No ) break;;
         *) echo "Invalid entry. Please type 1 for yes or type 2 for no."
     esac
@@ -19,7 +20,7 @@ done
 echo "Would you like me to help you setup ssh keys so you can connect to github?"
 select SSLGITHUB in "Yes" "No"; do
     case $SSLGITHUB in
-        Yes ) sh ssh_github.sh; break;;
+        Yes )sh ~/$CURRENT/ssh_github.sh; break;;
         No ) break;;
         *) echo "Invalid entry. Please type 1 for yes or type 2 for no."
     esac
@@ -29,7 +30,7 @@ done
 echo "Do you wish to clone a repo? Inorder to use this feuter your ec2 must be connected"
 select REPO in "Yes" "No"; do
     case $REPO in
-        Yes ) sh repo.sh; break;;
+        Yes )sh ~/$CURRENT/repo.sh; break;;
         No ) break;;
         *) echo "Invalid entry. Please type 1 for yes or type 2 for no."
     esac
@@ -39,7 +40,7 @@ done
 echo "Do you wish to install nginx? To use this feture you must have a build file"
 select NGINX in "Yes" "No"; do
     case $NGINX in
-        Yes ) sh nginx.sh; break;;
+        Yes )sh ~/$CURRENT/nginx.sh; break;;
         No ) break;;
         *) echo "Invalid entry. Please type 1 for yes or type 2 for no."
     esac
