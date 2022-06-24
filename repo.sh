4234d3899fa564625b47394ab7eb283609edff8b
KidusTadesseCode <Kidustadessearega@gmail.com>
2022-06-24 18:53:40 -0400

    dirname mod

--- repo.sh
+++ repo.sh
@@ -1,16 +1,17 @@
 # echo "Whats the repo ssl link?"
 # read REPOLINK
 
-while [ -z $REPOLINK ]
-do
-   echo "Whats the repo ssl link?"
-   read REPOLINK
-done
+# while [ -z $REPOLINK ]
+# do
+#    echo "Whats the repo ssl link?"
+#    read REPOLINK
+# done
 
 
 
 cd ~/
-git clone REPOLINK
+# git clone REPOLINK
+git clone git@github.com:KidusTadesseCode/navigation.git
 ls 
 FILEPROJECT=$(ls -td -- * | head -n 1)
 
