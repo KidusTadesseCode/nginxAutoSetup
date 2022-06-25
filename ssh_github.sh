5934b225b587c7ff62dd21e70aed574d9e4b9899
KidusTadesseCode <Kidustadessearega@gmail.com>
2022-06-25 00:04:23 -0400

    dirname mod

--- ssh_github.sh
+++ ssh_github.sh
@@ -6,13 +6,13 @@ If you provide passphrase, please make sure you remember it."
 read PASSPHRASE
 
 IDFIleNAME=$USERNAME"_id_rsa"
-ssh-keygen -f ~/.ssh/$IDFIleNAME -t rsa -N "$PASSPHRASE";
+sudo ssh-keygen -f ~/.ssh/$IDFIleNAME -t rsa -N "$PASSPHRASE";
 #sudo chmod 400 ~/.ssh/$IDFIleNAME
 echo -e '#'$USERNAME'\nHost '$USERNAME'\n\tHostName github.com\n\tIdentityFile ~/.ssh/'$IDFIleNAME>> ~/.ssh/config
 
-chmod go-w ~/.ssh/config
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/$IDFIleNAME
+sudo chmod go-w ~/.ssh/config
+sudo eval "$(ssh-agent -s)"
+sudo ssh-add ~/.ssh/$IDFIleNAME
 cat ~/.ssh/config
 
 echo "copy your public key and past it in your github account in your setting where it says SSH and GPG keys"
