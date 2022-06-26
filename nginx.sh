5b18d4aebc41e16df0f01704b5e8e02fe577248a
KidusTadesseCode <Kidustadessearega@gmail.com>
2022-06-26 00:49:53 -0400

    dirname mod

--- nginx.sh
+++ nginx.sh
@@ -19,16 +19,18 @@ function siteSetup(){
 # export DIRNAME
 # export SITE
 
-echo "is this a React or Node Backend App?"
-select APPTYPE in "React" "Node-Backend-App"; do
-    case $APPTYPE in
-        React ) sh reactbuild.sh; break;;
-        Node-Backend-App ) sudo cp -r ~/DIRNAME/*  ~/../../usr/share/nginx/html/$SITE; break;;
-        *) echo "Invalid entry. Please type 1 for React or type 2 for Node-Backend-App followed my Enter."
-    esac
-done
+function builderNG(){
+    echo "is this a React or Node Backend App?"
+    select APPTYPE in "React" "Node-Backend-App"; do
+        case $APPTYPE in
+            React ) sh reactbuild.sh; break;;
+            Node-Backend-App ) sudo cp -r ~/DIRNAME/*  ~/../../usr/share/nginx/html/$SITE; break;;
+            *) echo "Invalid entry. Please type 1 for React or type 2 for Node-Backend-App followed my Enter."
+        esac
+    done
 
-sh nginx.conf.sh
+    sh nginx.conf.sh
 
-sudo service nginx stop
-sudo service nginx start
+    sudo service nginx stop
+    sudo service nginx start
+}
