da6f741c9b021223e98bcaf4b3401636deba3350
KidusTadesseCode <Kidustadessearega@gmail.com>
2022-06-26 19:44:01 -0400

    dirname mod

--- repo.sh
+++ repo.sh
@@ -7,39 +7,11 @@ do
 done
 
 
-# eval "$(ssh-agent -s)"
-# cd 
-# git clone REPOLINK
+#git clone $REPOLINK ./ 
+git clone git@github.com:KidusTadesseCode/navigation.git ~/repo
+echo "git@github.com:KidusTadesseCode/navigation.git"
 
-# eval `grep "^export " ../ssh_github.sh`
-
-# mkdir ~/repo
-# eval "$(ssh-agent -s)"
-# ssh-add ~/.ssh/$IDFIleNAME
-# echo "from repo -- ${IDFIleNAME}"
-git clone $REPOLINK ~/
-
-# ssh-add ~/.ssh/try_id_rsa
-# git clone git@github.com:KidusTadesseCode/navigation.git ~/repo
-
-# eval "$(pwd)"
-
-# eval "$(ls ~/)"
-
-# echo "press enter key to continue "
-# read ANYKEY
-
-
-# FILEPROJECT=$(ls -td -- * | head -n 1)
-
-#cd FILEPROJECT/
-# pwd
-# cd ~/navigation
-# npm i
-
-# FOLDERNAME=${PWD##*/}
-# DIRNAME==$(pwd)
-echo $REPOLINK
+# echo $REPOLINK
 return
 # export FOLDERNAME
 # export DIRNAME
